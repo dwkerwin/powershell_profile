3f145f0f43aba6f64857f3f26fe97640a20c6e57
Doug Kerwin <dwkerwin@gmail.com>
2016-03-11 11:31:38 -0500

    added LaunchTime and sorting to List-EC2InstancesByName

--- autoload-scripts/awshelpers.ps1
+++ autoload-scripts/awshelpers.ps1
@@ -60,17 +60,20 @@ function RDP-EC2Instance($instanceId, $pw, [switch]$privateIp) {
 set-alias rdpe RDP-EC2Instance
 
 # returns instances only for any matching the name (case sensitive)
-function Get-EC2InstancesByName ($name)
+function List-EC2InstancesByName ($name)
 {
     $instanceList = aws ec2 describe-instances --filters "Name=tag:Name,Values=*$name*" | ConvertFrom-Json
 
-    $runningInstances = $instanceList.Reservations.Instances | ` #where { $_.State.Name -eq "running" } | `
-        select InstanceId, InstanceType, VpcId, State, Tags
+    $runningInstances = $instanceList.Reservations.Instances | `
+        # where { $_.State.Name -eq "running" } | `
+        select InstanceId, InstanceType, VpcId, State, LaunchTime, Tags | `
+        sort VpcId | `
+        sort LaunchTime -desc
 
     if ($runningInstances.Count -eq 0) {
-        echo "No matching instances.  Remember tag name compare is case sensitive."
+        echo "No matching instances.  Remember tag name compare is case sensitive :("
     } else {
         $runningInstances | ft -auto
     }
 }
-set-alias liste Get-EC2InstancesByName
+set-alias liste List-EC2InstancesByName
