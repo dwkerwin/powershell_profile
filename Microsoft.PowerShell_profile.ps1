4f7482fd1495846b6bde2e998f0f348c9a4f6816
Doug Kerwin <dwkerwin@yahoo.com>
2016-03-12 15:36:21 -0500

    secrets file inclusion fix

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,11 @@
 ################################################################################
 # Powershell Profile
 #
-# if a secrets file exists, run that first
-# when setting variables in the secrets file, to overcome script scope,
-# use $Global: syntax, # so to set $myvar = "", instead use $Global:myvar = ""
 
+# if a secrets file exists, include that first
 $secretsScript = Join-Path (Split-Path $PROFILE) "\secrets.ps1"
 if (Test-Path $secretsScript ) {
-    & $secretsScript
+    . $secretsScript
 }
 
 # auto load all powershell modules and scripts in the autoload directory
